abc3f190be3a31aa473f26238a588c13391817b3
Ivan <i.leonchik@gmail.com>
2017-05-03 10:15:15 +0200

    Add WPF UI

---
+++ Test/Test.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.WpfGraphControl;
+
+namespace Test.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DockPanel Panel = new DockPanel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Content = Panel;
+            Loaded += Window_Loaded;
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GraphViewer graphViewer = new GraphViewer();
+            graphViewer.BindToPanel(Panel);
+            Graph graph = new Graph();
+
+            graph.AddEdge("A", "B");
+            graph.Attr.LayerDirection = LayerDirection.LR;
+            graphViewer.Graph = graph; // throws exception
+        }
+    }
+}

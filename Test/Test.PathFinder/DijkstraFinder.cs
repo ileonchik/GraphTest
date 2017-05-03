70e03b9321dbf529511194ca934cfc6eaebd9e87
Ivan <i.leonchik@gmail.com>
2017-05-03 13:32:51 +0200

    Add finder factory + Dijkstra implementation

--- Test/Test.PathFinder/DijkstraFinder.cs
+++ Test/Test.PathFinder/DijkstraFinder.cs
@@ -8,12 +8,81 @@ using Test.Common.Interfaces;
 
 namespace Test.PathFinder
 {
-    public class DijkstraFinder : IFinder
+    public class DijkstraFinder : BaseFinder
     {
-        public List<string> Find(string start, string stop)
+        public DijkstraFinder(Graph graph) : base(graph)
         {
-            throw new NotImplementedException();
+        }
+
+        protected override List<int> Find(int[,] adjaciesMatrix, int start, int stop, int size)
+        {
+            var way = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                way[i] = int.MaxValue;
+            }
+
+            way[start] = 0;
+
+            var visited = new bool[size];
+            var previous = new int?[size];
+
+            while (true)
+            {
+                var minDistance = int.MaxValue;
+                var minNode = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    if (!visited[i] && minDistance > way[i])
+                    {
+                        minDistance = way[i];
+                        minNode = i;
+                    }
+                }
+
+                if (minDistance == int.MaxValue)
+                {
+                    break;
+                }
+
+                visited[minNode] = true;
+
+                for (int i = 0; i < size; i++)
+                {
+                    if (adjaciesMatrix[minNode, i] > 0)
+                    {
+                        var shortestToMinNode = way[minNode];
+                        var distanceToNextNode = adjaciesMatrix[minNode, i];
+
+                        var totalDistance = shortestToMinNode + distanceToNextNode;
+
+                        if (totalDistance < way[i])
+                        {
+                            way[i] = totalDistance;
+                            previous[i] = minNode;
+                        }
+                    }
+                }
+            }
+
+            if (way[stop] == int.MaxValue)
+            {
+                return null;
+            }
+
+            var path = new LinkedList<int>();
+            int? currentNode = stop;
+            while (currentNode != null)
+            {
+                path.AddFirst(currentNode.Value);
+                currentNode = previous[currentNode.Value];
+            }
+
+            return path.ToList();
         }
     }
 
+
 }
+
+
---
+++ Test/Test.PathFinder/FinderFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Common.Enities;
+using Test.Common.Interfaces;
+
+namespace Test.PathFinder
+{
+    public class FinderFactory
+    {
+        public static IFinder GetFinder(FinderType finderType,Graph graph)
+        {
+            switch (finderType)
+            {
+                case FinderType.Default:
+                    return new DefaultFinder(graph);
+                case FinderType.Dijkstra:
+                    return new DijkstraFinder(graph);
+                default:
+                    throw  new ArgumentException(String.Format("Finder type {0} is not supported",finderType));
+            }
+        }
+    }
+}
